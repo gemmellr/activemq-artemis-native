863377e9852e35c3b8995f8f80ba5da145bd422b
Robbie Gemmell <robbie@apache.org>
2022-05-23 16:09:41 +0100

    fixups

--- scripts/compile-using-docker.sh
+++ scripts/compile-using-docker.sh
@@ -16,13 +16,20 @@
 # specific language governing permissions and limitations
 # under the License.
 
-if [ -d "./bin" ]; then rm -Rf ./bin; fi
+if [ -d "./target" ]; then
+  if [ -d "./target/lib" ]; then
+     rm -rf "./target/lib"
+  fi
+  mkdir "target/lib"
+else
+  mkdir -p "target/lib"
+fi
+
 docker build -f src/main/docker/Dockerfile-centos -t artemis-native-builder .
 docker run --rm -v $PWD/target/lib:/work/target/lib artemis-native-builder "$@"
 #podman run --rm -v $PWD/target/lib:/work/target/lib:Z artemis-native-builder "$@"
-chown -Rv $USER:$GID ./bin
+chown -Rv $USER:$GID ./target/lib
 ls -liat ./target/lib
-#TODO: I had to manually create ./target/lib before I could run this..
 
 # Note: You may need to authorize docker to map folder at your folder structure
 #docker build -f src/main/docker/Dockerfile-centos -t artemis-native-builder . && docker run -it --rm -v $PWD/target/lib:/work/target/lib artemis-native-builder bash
